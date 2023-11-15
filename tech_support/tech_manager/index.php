88cba0e07f19c455417c8100e6aada3d450053d5
LongTangGithub <tanglh@g.cofc.edu>
2023-11-15 02:18:58 -0500

    second tech-db changes

--- tech_support/tech_manager/index.php
+++ tech_support/tech_manager/index.php
@@ -1,56 +1,72 @@
 <?php
-require('database_oo.php');
-require('technician.php');
 
+require('../model/database_oo.php');
+require('../model/technician.php');
+require('../model/technician_db_oo.php');
 
-// Create a new database ojbect and connect via function defined in database_oop.php
-$db = new database();
-$db = $db->connect();
+// Create link to technician DB methods
+$technicianDB = new TechnicianDB();
 
-require('../model/technician_db.php');
-
-
+//Create an action that will filter through user buttons then call another function
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action === NULL) {
-        $action = 'list_technicians';
+        $action = 'manage_technicians';
     }
 }
 
-if ($action == 'list_technicians') {
-    // Get technician data display in table
-    $technicians = get_technicians();
-
-    // Display the technician list
+//Display the technicians list
+if ($action == 'manage_technicians') {
+    // Retrieve all technicians
+    $technicians = $technicianDB->getTechnicians();
     include('technician_list.php');
-} else if ($action == 'delete_technician') {
-    $technician_id = filter_input(INPUT_POST, 'technician_id', FILTER_VALIDATE_INT);
-    delete_technician($technician_id);
-    header("Location: .");
-} else if ($action == 'show_add_form') {
-    include('technician_add.php');
-} else if ($action == 'add_technician') {
-    $first_name = filter_input(INPUT_POST, 'first_name');
-    $last_name = filter_input(INPUT_POST, 'last_name');
+}
+
+//Delete the technician that was clicked on
+else if ($action == 'delete_technician') {
+    $tech_id = filter_input(INPUT_POST, 'tech_id');
+    if ($tech_id == NULL || $tech_id == FALSE) {
+        $error = "Missing or incorrect product id or category id.";
+        include('../errors/error.php');
+    } else { 
+        // Delete specified technician
+        $technicianDB->deleteTechnician($tech_id);
+        header('Location: .?action=manage_technicians'); //Display the technicians page
+    }
+}
+
+//Show the add technician form
+else if ($action == 'show_add_form') {
+    include('technician_add.php');    
+}
+
+//Add a new technician with the credentials passed through
+else if ($action == 'add_technician') {
+    // Store input values into variable
+    $first_name = filter_input(INPUT_POST, 'firstName');
+    $last_name = filter_input(INPUT_POST, 'lastName');
+    $email = filter_input(INPUT_POST, 'email');
     $phone = filter_input(INPUT_POST, 'phone');
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
     $password = filter_input(INPUT_POST, 'password');
 
-    // Validating all inputs
-    if (empty($first_name) || empty($last_name) || empty($phone) || 
-        $email === FALSE || empty($password)) {
-            $error = "Invalid technician data. Check all fields and try again.";
-            include('../errors/error.php');
+    if ($first_name == NULL || $last_name == NULL || $email == NULL || $phone == NULL || $password == NULL) {
+        $error = "Invalid technician data. Check all fields and try again.";
+        include('../errors/error.php');
+    } else { 
+        // Create a new technician object with no arguments
+        $technician = new Technician();
+        // Assign technician properties per input variables
+        $technician->setFirstName($first_name);
+        $technician->setLastName($last_name);
+        $technician->setEmail($email);
+        $technician->setPhone($phone);
+        $technician->setPassword($password);
 
-    }else{
-        // Execute the add_technician method and display it the table
-        add_technician($technician_id, $first_name, $last_name, $email, $phone, $password);
-        header("Location: .");
+        // Add the individual properties to the new technician object
+        $technicianDB->addTechnician($technician);
 
+        header('Location: .?action=manage_technicians'); //Display the products page
     }
 }
-
-
-
 ?>
--- tech_support/tech_manager/technician_list.php
+++ tech_support/tech_manager/technician_list.php
@@ -1,34 +1,36 @@
 <?php include '../view/header.php'; ?>
 <main>
     <h1>Technician List</h1>
-
-    <!-- display a table of products -->
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Password</th>
-            <th>&nbsp;</th>
-        </tr>
-        <?php foreach ($technicians as $technician) : ?>
-        <tr>
-            <td><?php echo Technician::get_names($technician); ?></td>
-            <td><?php echo htmlspecialchars($technician['email']); ?></td>
-            <td><?php echo htmlspecialchars($technician['phone']); ?></td>
-            <td><?php echo htmlspecialchars($technician['password']); ?></td>
-
-            <td>
-                <form action="." method="post">
+    <section>
+        <!-- display a table of technicians -->
+        <table>
+            <tr><?php // remember the first and last name columns should be combined into one ?>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Password</th>
+                <th>&nbsp;</th>
+            </tr>
+            <?php foreach ($technicians as $technician) : ?>
+            <tr>
+                <?php
+                // Remember to replace the query results with method calls
+                // to get properties
+                ?>
+                <td><?php echo $technician->getFullName(); ?></td>
+                <td><?php echo $technician->getEmail(); ?></td>
+                <td><?php echo $technician->getPhone(); ?></td>
+                <td><?php echo $technician->getPassword(); ?></td>
+                <td><form action="." method="post">
                     <input type="hidden" name="action" value="delete_technician">
-                    <input type="hidden" name="technician_id" value="<?php echo htmlspecialchars($technician['techID']); ?>">
+                    <input type="hidden" name="tech_id" value="<?php echo $technician->getID(); ?>">
                     <input type="submit" value="Delete">
-                </form>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-    <p><a href="?action=show_add_form">Add Technician</a></p>
-
+                </form></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <p><a href="index.php?action=show_add_form">Add Technician</a></p>
+        </p>             
+    </section>
 </main>
 <?php include '../view/footer.php'; ?>
